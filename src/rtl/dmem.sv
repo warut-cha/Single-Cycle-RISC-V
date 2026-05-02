4a4954b099bf5e6338b50d8463dc4d8225f78059
warut-cha <warre@W5CG5073WR2.localdomain>
2026-05-03 01:37:26 +0200

    Bug in dmem and imem

--- src/rtl/dmem.sv
+++ src/rtl/dmem.sv
@@ -9,6 +9,7 @@ module dmem (
     reg [31:0] ram[0:1023]; // 1924 memory slots (4kb RAM)
 
     // same need 10 bits
+    wire unused_address_bits = |{address[31:12], address[1:0]}; // Check if any of the upper bits are used
     assign read_data = ram[address[11:2]]; // Address is word-aligned, so we ignore the 2 least significant bits
     always @(posedge clk) begin
         if(write_enable == 1'b1) begin
--- src/rtl/imem.sv
+++ src/rtl/imem.sv
@@ -3,13 +3,14 @@ module imem(
     output wire [31:0] instr //32-bit instruction
 );
 
-    reg[31:00] rom[0:1023]; //1024 words of 32-bit instruction memory
+    reg[31:0] rom[0:1023]; //1024 words of 32-bit instruction memory
 
     initial begin
         //read text file and load it into array automatically
         $readmemh("src/txt/program.hex", rom);
     end
 
+    wire unused_address_bits = |{address[31:12], address[1:0]}; // Check if any of the upper bits are used
     //1024 needs 10 bits
     assign instr = rom[address[11:2]]; //word aligned, so ignore the last 2 bits
 
